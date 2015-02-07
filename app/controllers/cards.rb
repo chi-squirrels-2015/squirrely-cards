ae9212a5c6d70c9f075df171027cd636d2a8bed2
Matthew Gray <graymatthew3@gmail.com>
2015-02-06 19:18:53 -0600

    MVP baby, MVP

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -1,26 +1,38 @@
-get "/cards" do
-  @cards = Cards.all
-  erb :"cards/index"
-end
+# get "/cards" do
+#   @cards = Cards.all
+#   erb :"cards/index"
+# end
 
-get "/decks/:name/cards/:id" do
-  @card = Card.find(params[:id])
-  erb :"cards/show"
-end
-
-post "/deck/:name/cards/:id/guess" do
+get "/decks/:deck_id/cards/:id" do
   @card = Card.find(params[:id])
   deck_of_cards = @card.deck.cards
   current_card_index = deck_of_cards.index(@card)
-  next_card_id = deck_of_cards.at(current_card_index + 1).id
+  next_card = deck_of_cards.at(current_card_index + 1)
+
+  @next_card_id = next_card.id if next_card
+
+  erb :"cards/show"
+end
+
+post "/decks/:deck_id/cards/:id/guess" do
+  @card = Card.find(params[:id])
+  deck_of_cards = @card.deck.cards
+  current_card_index = deck_of_cards.index(@card)
+  next_card = deck_of_cards.at(current_card_index + 1)
+
+  @next_card_id = next_card.id if next_card
 
   guess = params[:guess]
 
-  if @card.answer == guess
-    session[:message] = "You guessed correct!"
-  else
-    session[:message] = "You guessed #{guess}, but the correct answer was #{@card.answer}."
+  if guess
+    @correct_answer = (@card.answer == guess)
   end
 
-  redirect "/deck/#{@card.deck.name}/cards/#{next_card_id}"
+  erb :"cards/show"
+
+  # if next_card
+  #   redirect "/decks/#{@card.deck.id}/cards/#{next_card.id}"
+  # else
+  #   erb :"decks/finish"
+  # end
 end
